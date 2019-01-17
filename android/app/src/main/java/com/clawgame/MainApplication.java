5a56bd8f31f5d2b54c558584cadb62ec0d81185d
karn806 <nattakarn.kl@gmail.com>
2019-01-17 20:36:24 +0700

    able to connect to BLE HM10

--- android/app/src/main/java/com/clawgame/MainApplication.java
+++ android/app/src/main/java/com/clawgame/MainApplication.java
@@ -3,7 +3,9 @@ package com.clawgame;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rusel.RCTBluetoothSerial.RCTBluetoothSerialPackage;
 import com.polidea.reactnativeble.BlePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.solinor.bluetoothstatus.RNBluetoothManagerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTBluetoothSerialPackage(),
             new BlePackage(),
+            new VectorIconsPackage(),
             new RNBluetoothManagerPackage()
       );
     }
--- src/App.js
+++ src/App.js
@@ -6,70 +6,138 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    FlatList,
+    Switch,
+    TouchableOpacity,
+    ToastAndroid} from 'react-native';
+// import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { Col, Row, Grid } from "react-native-easy-grid"
+import AwesomeButton from 'react-native-really-awesome-button';
 import { BleManager } from 'react-native-ble-plx';
 
-type Props = {};
-export default class App extends Component {
+export default class App extends React.Component {
 
     constructor() {
         super();
         this.manager = new BleManager();
-    }
-
-    static propTypes = {
-        deviceName: PropTypes.string,
-    };
-
-    state = {
-        deviceName: ''
+        this.device = null;
     }
 
     componentWillMount() {
         const subscription = this.manager.onStateChange((state) => {
-            console.log('component will mount');
             if (state === 'PoweredOn') {
-                console.log('power on');
                 this.scanAndConnect();
                 subscription.remove();
             }
         }, true);
     }
 
-    scanAndConnect() {
+    scanAndConnect = () => {
         this.manager.startDeviceScan(null, null, (error, device) => {
+
             if (error) {
                 // Handle error (scanning will be stopped automatically)
                 return
             }
 
-            console.log("device name:")
-            console.log(device.name);
-
             // Check if it is a device you are looking for based on advertisement data
             // or other criteria.
-            if (device.name === 'TI BLE Sensor Tag' ||
-                device.name === 'SensorTag') {
+            if (device.name === 'JDY-09-V4.3') {
 
                 // Stop scanning as it's not necessary if you are scanning for one device.
                 this.manager.stopDeviceScan();
+                this.device = device;
 
                 // Proceed with connection.
             }
         });
     }
 
+    connect = () => {
+        this.device.connect()
+            .then((device) => {
+                return device.discoverAllServicesAndCharacteristics()
+            })
+            .then((device) => {
+                // Do work on device with services and characteristics
+            })
+            .catch((error) => {
+                // Handle errors
+            });
+    }
+
+    disconnect = () => {
+        this.manager.cancelDeviceConnection(this.device.id);
+        this.device = null;
+    }
+
     render() {
-        const {
-            deviceName
-        } = this.state;
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    <p>Welcome to React Native!</p>
-                    <p>Device name: {deviceName}</p>
-                </Text>
+                <View style={styles.toolbar}>
+                    <Text style={styles.toolbarTitle}>Bluetooth Device List</Text>
+                </View>
+                <Button
+                    raised
+                    onPress={this.connect}
+                    title="Connect"
+                    color="#841584"
+                />
+
+                <Button
+                    raised
+                    onPress={this.disconnect}
+                    title="Disconnect"
+                />
+
+
+                {/*<Grid style={styles.textContainer}>*/}
+                    {/*<Text style={styles.inputText}>*/}
+                        {/*READY!*/}
+                    {/*</Text>*/}
+                {/*</Grid>*/}
+                {/*<Grid>*/}
+                    {/*<Col style={styles.leftBox}>*/}
+                        {/*<View style={styles.controlBtn}>*/}
+                            {/*<View style={{flex: 1, flexDirection: 'row'}}>*/}
+                                {/*<View></View>*/}
+                                {/*<Button danger>*/}
+                                    {/*<Text> ^ </Text>*/}
+                                {/*</Button>*/}
+                                {/*<View></View>*/}
+                            {/*</View>*/}
+                            {/*<View style={{flex: 1, flexDirection: 'row'}}>*/}
+                                {/*<Button danger>*/}
+                                    {/*<Text> LEFT </Text>*/}
+                                {/*</Button>*/}
+                                {/*<View style={{width: 80, height: 50}} />*/}
+                                {/*<Button danger>*/}
+                                    {/*<Text> RIGHT </Text>*/}
+                                {/*</Button>*/}
+                            {/*</View>*/}
+                            {/*<View style={{flex: 1, flexDirection: 'row'}}>*/}
+                                {/*<View></View>*/}
+                                {/*<Button danger>*/}
+                                    {/*<Text> v </Text>*/}
+                                {/*</Button>*/}
+                                {/*<View></View>*/}
+                            {/*</View>*/}
+                        {/*</View>*/}
+                    {/*</Col>*/}
+                    {/*<Col style={styles.rightBox}>*/}
+                        {/*<View style={styles.grabBtn}>*/}
+                            {/*<Button danger large>*/}
+                                {/*<Text> GRAB !!! </Text>*/}
+                            {/*</Button>*/}
+                        {/*</View>*/}
+                    {/*</Col>*/}
+                {/*</Grid>*/}
             </View>
         );
     }
@@ -78,9 +146,27 @@ export default class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // backgroundColor: '#F5FCFF',
+    },
+    textContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+    },
+    inputText: {
+      fontSize: 40,
+    },
+    grabBtn: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#EB5EBC',
+    },
+    controlBtn: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#F7DC2B',
     },
     welcome: {
         fontSize: 20,
@@ -92,4 +178,46 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    leftBox: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#F7DC2B',
+    },
+    rightBox: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#EB5EBC',
+    },
+    gridContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    toolbar:{
+        paddingTop:30,
+        paddingBottom:30,
+        flexDirection:'row'
+    },
+    toolbarButton:{
+        width: 50,
+        marginTop: 8,
+    },
+    toolbarTitle:{
+        textAlign:'center',
+        fontWeight:'bold',
+        fontSize: 20,
+        flex:1,
+        marginTop:6
+    },
+    deviceName: {
+        fontSize: 17,
+        color: "black"
+    },
+    deviceNameWrap: {
+        margin: 10,
+        borderBottomWidth:1
+    }
 });
+
